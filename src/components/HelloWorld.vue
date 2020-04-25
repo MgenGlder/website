dc7a2220b5144567496f43b4a7f4c0c5fdc0ca7c
Kunle Oshiyoye <mgen.glder@gmail.com>
2020-04-25 16:44:22 -0400

    Initial changes, just to make sure that things work

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
-  <h1>Jekyll-Vue Template</h1>
+  <div>
+    <h1>Jekyll-Vue Template</h1>inside the vue template...
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
-}
+  name: "HelloWorld"
+};
 </script>
 
 <style lang="scss" scoped>
